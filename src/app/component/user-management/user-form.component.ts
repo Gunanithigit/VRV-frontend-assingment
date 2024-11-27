59b4d8965dc84d0d4a5e8b549922d96752958731
Gunanithigit <gunanithi888@gmail.com>
2024-11-27 22:31:23 +0530

    Update user-form.component.ts

--- src/app/component/user-management/user-form.component.ts
+++ src/app/component/user-management/user-form.component.ts
@@ -1,1 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent implements OnInit {
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      age: ['', [Validators.required, Validators.min(18)]],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.userForm.valid) {
+      console.log('User Data:', this.userForm.value);
+      alert('Form Submitted Successfully!');
+      this.userForm.reset(); // Reset the form after submission
+    } else {
+      alert('Please correct the errors in the form.');
+    }
+  }
+}
 
