cf52530d628cb102a4a38cf3bdf08e34e8e229f2
Gunanithigit <gunanithi888@gmail.com>
2024-11-27 23:09:01 +0530

    Update role.service.ts

--- src/app/component/services/role.service.ts
+++ src/app/component/services/role.service.ts
@@ -1,1 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Role } from '../models/role.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RoleService {
+  private apiUrl = '/api/roles';
+
+  constructor(private http: HttpClient) {}
+
+  getRoles(): Observable<Role[]> {
+    return this.http.get<Role[]>(this.apiUrl);
+  }
+
+  createRole(role: Role): Observable<Role> {
+    return this.http.post<Role>(this.apiUrl, role);
+  }
+
+  updateRole(role: Role): Observable<Role> {
+    return this.http.put<Role>(`${this.apiUrl}/${role.id}`, role);
+  }
+
+  deleteRole(roleId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${roleId}`);
+  }
+}
 
