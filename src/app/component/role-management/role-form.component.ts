f9c09f8021bca7a56d0e1a53461ef9f026977dd2
Gunanithigit <gunanithi888@gmail.com>
2024-11-27 22:51:20 +0530

    Update role-form.component.ts

--- src/app/component/role-management/role-form.component.ts
+++ src/app/component/role-management/role-form.component.ts
@@ -1,1 +1,41 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-role-form',
+  templateUrl: './role-form.component.html',
+  styleUrls: ['./role-form.component.css']
+})
+export class RoleFormComponent implements OnInit {
+  @Input() role: { id?: number; name: string; description: string } | null = null;
+  @Output() saveRole = new EventEmitter<{ id?: number; name: string; description: string }>();
+  @Output() cancel = new EventEmitter<void>();
+
+  roleForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.roleForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required, Validators.minLength(10)]]
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.role) {
+      this.roleForm.patchValue(this.role);
+    }
+  }
+
+  onSubmit(): void {
+    if (this.roleForm.valid) {
+      this.saveRole.emit({ id: this.role?.id, ...this.roleForm.value });
+      this.roleForm.reset();
+    }
+  }
+
+  onCancel(): void {
+    this.cancel.emit();
+    this.roleForm.reset();
+  }
+}
 
